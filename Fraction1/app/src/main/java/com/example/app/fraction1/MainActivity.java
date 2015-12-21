1779e606a9744547da712fbb0609100c771e1a12
dianaRusak <dianka23@inbox.ru>
2015-12-21 13:46:28 +1000

    fraction

---
+++ Fraction1/app/src/main/java/com/example/app/fraction1/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.app.fraction1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import static android.widget.Toast.*;
+
+public class MainActivity extends AppCompatActivity {
+    public int summZn, summCh, chO, znO, ch1, zn1;
+    int gcd (int a, int b) {
+        if (b == 0)
+            return a;
+        else
+            return gcd (b, a % b);
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button1 = (Button) findViewById(R.id.button);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText editText = (EditText) findViewById(R.id.editText);
+                EditText editText1 = (EditText) findViewById(R.id.editText2);
+                EditText editText2 = (EditText) findViewById(R.id.editText3);
+                EditText editText3 = (EditText) findViewById(R.id.editText4);
+                String a = editText.getText().toString();
+                chO = Integer.parseInt(a);
+                String b = editText1.getText().toString();
+                znO = Integer.parseInt(b);
+                String c = editText2.getText().toString();
+                ch1 = Integer.parseInt(c);
+                String d = editText3.getText().toString();
+                zn1 = Integer.parseInt(d);
+                TextView text = (TextView) findViewById(R.id.textView4);
+                TextView text1 = (TextView) findViewById(R.id.textView5);
+                Spinner spinner = (Spinner) findViewById(R.id.spinner);
+                String selected = spinner.getSelectedItem().toString();
+                summZn = gcd(znO, zn1) ;
+                text1.setText(summZn);
+                switch (selected) {
+                    case "summation":
+                        summCh = summZn / znO * chO + summZn / zn1 * ch1;
+                        text.setText(summCh);
+                        break;
+
+                    case "subtractionn":
+                        summCh = ch1 * chO;
+                        text.setText(summCh);
+                        break;
+
+                    case "division":
+                        summCh = chO * zn1;
+                        text.setText(summCh);
+                        break;
+
+                    case "multiplicatio":
+                        summCh = summZn / znO * chO - summZn / zn1 * ch1;
+                        text.setText(summCh);
+                        break;
+                }
+            }
+        });
+    }
+}
