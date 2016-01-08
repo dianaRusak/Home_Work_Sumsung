7d97d0676d7bc72d0da3bfc45a7d91886b6d9b0d
dianaRusak <dianka23@inbox.ru>
2016-01-09 00:28:48 +1000

    3/3. It`s win

---
+++ MyApplication/app/src/main/java/com/example/app/myapplication/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.app.myapplication;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Получим ссылку на солнце
+        ImageView sunImageView = (ImageView) findViewById(R.id.sun);
+        // Анимация для восхода солнца
+        Animation sunRiseAnimation = AnimationUtils.loadAnimation(this, R.anim.sun_rise);
+        // Подключаем анимацию к нужному View
+        sunImageView.startAnimation(sunRiseAnimation);
+    }
+}
